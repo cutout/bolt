0bde6871dd65c63e2db328abe5a8cd4c57233f9b
cutout <cutout@gmail.com>
2014-11-27 13:28:37 -0500

    1280

--- content-postmetadata.php
+++ content-postmetadata.php
@@ -2,7 +2,7 @@
 		<em>
 			<?php _e('By','gravy'); ?>
 		</em> 
-		<?php the_author_posts_link('namefl'); ?> <?php _e('on','gravy'); ?> <time><?php the_time('M d, Y'); ?></time>
+		<?php the_author_posts_link('namefl'); ?> <?php _e('on','gravy'); ?> <time><?php the_time('F d, Y'); ?></time>
 		&bull; 
 		<span class="commentcount">
 			(<?php comments_popup_link('0', '1', '%'); ?>)
